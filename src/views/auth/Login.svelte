9fdcdf269fb9cffacbdd803a1b762f9adeae3e47
Rithikun Sen <s.rithikun99@gmail.com>
2024-05-15 13:40:05 +1000

    Merge (prototype-change) branch

--- src/views/auth/Login.svelte
+++ src/views/auth/Login.svelte
@@ -1,10 +1,50 @@
 <script>
   import { link } from "svelte-routing";
+  import { userLogin } from "../api/user";
+
+  let details = {
+    email: "",
+    password: "",
+  };
+  let errors = {
+    email: "",
+    password: "",
+  };
+  let valid = false;
 
   // core components
   const github = "../assets/img/github.svg";
   const google = "../assets/img/google.svg";
   export let location;
+
+  //user submit handler and form validation
+  const submitHandler = () => {
+    valid = true;
+    //validate email
+    if (details.email === "") {
+      errors.email = "Email is required";
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(details.email)) {
+      errors.email = "Email is invalid";
+      valid = false;
+    } else {
+      errors.email = "";
+    }
+    //validate password
+    if (details.password === "") {
+      errors.password = "Password is required";
+      valid = false;
+    } else {
+      errors.password = "";
+    }
+    if (!valid) {
+      return;
+    }
+    //submit user details to server
+    userLogin(details).then((res) => {
+      console.log(res);
+    });
+  };
 </script>
 
 <div class="container mx-auto px-4 h-full">
@@ -45,14 +85,16 @@
             class="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
             href="/"
           >
-          Floodtech
+            Floodtech
           </a>
         </div>
         <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
           <div class="text-blueGray-400 text-center mb-3 font-bold">
-            <br><p>Secure Sign-in</p>
+            <br />
+            <p>Secure Sign-in</p>
+            <!-- <p>{test}</p> -->
           </div>
-          <form>
+          <form on:submit|preventDefault={submitHandler}>
             <div class="relative w-full mb-3">
               <label
                 class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
@@ -62,10 +104,13 @@
               </label>
               <input
                 id="grid-email"
-                type="email"
+                type="text"
                 class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                 placeholder="Email"
+                name="email"
+                bind:value={details.email}
               />
+              <div class="error">{errors.email}</div>
             </div>
 
             <div class="relative w-full mb-3">
@@ -80,7 +125,9 @@
                 type="password"
                 class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                 placeholder="Password"
+                bind:value={details.password}
               />
+              <div class="error">{errors.password}</div>
             </div>
             <div>
               <label class="inline-flex items-center cursor-pointer">
@@ -98,7 +145,7 @@
             <div class="text-center mt-6">
               <button
                 class="bg-indigo-900 text-white active:bg-indigo-800 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                type="button"
+                type="submit"
               >
                 Sign In
               </button>
@@ -108,7 +155,11 @@
       </div>
       <div class="flex flex-wrap mt-6 relative">
         <div class="w-1/2">
-          <a href="#pablo" on:click={(e) => e.preventDefault()} class="text-blueGray-200">
+          <a
+            href="#pablo"
+            on:click={(e) => e.preventDefault()}
+            class="text-blueGray-200"
+          >
             <small>Forgot password?</small>
           </a>
         </div>
@@ -121,4 +172,3 @@
     </div>
   </div>
 </div>
-
